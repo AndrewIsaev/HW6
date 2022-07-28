485184dc0392b5bd3bac4c83c53fd1c31594bdf5
andrey.isaev <an.isaev7@gmail.com>
2022-07-28 09:46:23 +0300

    add docstrings

--- functions.py
+++ functions.py
@@ -4,8 +4,8 @@ import random
 def count_words_in_file(file_name: str):
     """
     Count quantity of lines on file
-    :param file_name:  name of file
-    :return: len`s quantity
+    :param file_name: name of file
+    :return: words quantity
     """
     counter_line = 0
     with open(file_name) as file:
@@ -17,8 +17,8 @@ def count_words_in_file(file_name: str):
 def shuffle_letters(word: str):
     """
     Shuffle letters in word
-    :param word: str
-    :return: str
+    :param word: word which we need to shuffle
+    :return: shuffle word
     """
     word_list = [symbol for symbol in word]
     random.shuffle(word_list)
@@ -30,9 +30,9 @@ def shuffle_letters(word: str):
 def chose_word(file_name: str, word_number: int):
     """
     Choose word from file with index
-    :param file_name: str
-    :param word_number: int
-    :return: str
+    :param file_name: name if file
+    :param word_number: word(line) number in file
+    :return: selected word
     """
     words = []
     with open(file_name, "r", encoding="utf-8") as file:
@@ -44,9 +44,9 @@ def chose_word(file_name: str, word_number: int):
 def write_history(name: str, score: int):
     """
     Write history to file
-    :param name: str
-    :param score: int
-    :return: None
+    :param name: username
+    :param score: game score
+    :return:
     """
     with open("history.txt", "a", encoding="utf-8") as stats:
         stats.write(f"{name} {score}\n")
@@ -55,8 +55,8 @@ def write_history(name: str, score: int):
 def get_statistics(file_name: str):
     """
     From file with history get amount of games and max score
-    :param file_name: str
-    :return:
+    :param file_name: filename
+    :return: quantity of games , max result
     """
     with open(file_name) as file:
         score_list = []
@@ -68,8 +68,8 @@ def get_statistics(file_name: str):
 def print_statistics(total_games: int, max_score: int):
     """
     Print statistic
-    :param total_games: int
-    :param max_score: int
+    :param total_games: quantity of games
+    :param max_score: max result
     """
     print(f"Всего игр сыграно: {total_games}")
     print(f"Максимальный рекорд: {max_score}")
--- main.py
+++ main.py
@@ -7,9 +7,11 @@ def main():
     WORDS_FILE = "words.txt"
     HISTORY_FILE = "history.txt"
     score = 0
+
     # name request
     print("Введите ваше имя")
     user_name = input()
+
     for i in range(count_words_in_file(WORDS_FILE)):
         print(f"Угадайте слово: {shuffle_letters(chose_word(WORDS_FILE, i))}")
         user_answer = input()
@@ -18,6 +20,8 @@ def main():
             print("Верно! Вы получаете 10 очков.")
         else:
             print(f"Неверно! Верный ответ – {chose_word(WORDS_FILE, i)}.")
+
+    # Write results in file and print statistic
     write_history(user_name, score)
     game_amount, max_total_score = get_statistics(HISTORY_FILE)
     print_statistics(game_amount, max_total_score)
